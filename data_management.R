92a9fd68e079d9950e0dae1868e53b654c33e979
BAlexSimmons <b.alexander.simmons@gmail.com>
2026-02-22 19:31:20 -0500

    added HTML export option for final community map

--- data_management.R
+++ data_management.R
@@ -105,7 +105,7 @@ tampabay_metrics_wgs84 <- st_transform(tampabay_metrics, crs = 4326)
 pal_burdens <- colorNumeric(palette = brewer.pal(8, "YlOrRd"), domain = tampabay_metrics_wgs84$total_burdens)
 
 # Plot the number of burdens identified, and show list of burdens upon click
-tampabay_metrics_wgs84 %>%
+map <- tampabay_metrics_wgs84 %>%
   leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(
@@ -128,4 +128,9 @@ community_map <- tampabay_metrics_wgs84
 #save(community_map, file = 'data/community_map.RData')
 #st_write(community_map, "data/community_map.shp", driver = "ESRI Shapefile", delete_dsn = TRUE)
 #st_write(community_map, "data/community_map.kml", driver = "KML", delete_dsn = TRUE)
+
+
+# Save the leaflet map as an HTML file that can be shared for easy viewing outside of R
+library(htmlwidgets)
+#saveWidget(map, "community_map.html", selfcontained = TRUE)
 
