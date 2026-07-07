711148e99e8491ae12ca329ff20a4c9afc0749b7
BAlexSimmons <b.alexander.simmons@gmail.com>
2026-07-07 13:21:42 -0400

    added second option for vulnerability methodology

--- data_management.R
+++ data_management.R
@@ -113,9 +113,12 @@ tampabay_metrics <- tampabay_metrics %>%
   relocate(density, .after = area_mi2)
 
 
-# ---- (PROPOSED) Create map of burdened communities ----
 
-tampabay_metrics <- tampabay_metrics %>%
+# ---- Create map of flood vulnerable communities ----
+
+# ---- * METHOD 1: Equal consideration of Demographic, Economic, Household, and Environmental Burdens ----
+
+tampabay_metrics1 <- tampabay_metrics %>%
   # calculate vulnerability index based on the weights applied to the 12 vulnerability criteria
   mutate(vulnerability = 
            (burden_income*1) + 
@@ -171,15 +174,15 @@ tampabay_metrics <- tampabay_metrics %>%
   relocate(vul_burdens, .after = vulnerability)
   
 # Convert to WGS84 for viewing with leaflet
-tampabay_metrics_wgs84 <- st_transform(tampabay_metrics, crs = 4326)
+tampabay_metrics1_wgs84 <- st_transform(tampabay_metrics1, crs = 4326)
 
 # Create color palette
-pal_vulnerability <- colorNumeric(palette = brewer.pal(8, "YlGnBu"), domain = tampabay_metrics_wgs84$vulnerability)
+pal_vulnerability <- colorNumeric(palette = brewer.pal(8, "YlGnBu"), domain = tampabay_metrics1_wgs84$vulnerability)
 pal_vulnerability <- colorNumeric(palette = c(brewer.pal(8, "YlGnBu"), "#000000"), domain = c(0, 8))
-#pal_vulnerability <- colorNumeric(plasma(256), domain = tampabay_metrics_wgs84$vulnerability)
+#pal_vulnerability <- colorNumeric(plasma(256), domain = tampabay_metrics1_wgs84$vulnerability)
 
 # Plot the number of burdens identified, and show list of burdens upon click
-map <- tampabay_metrics_wgs84 %>%
+map1 <- tampabay_metrics1_wgs84 %>%
   leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(
@@ -222,55 +225,141 @@ map <- tampabay_metrics_wgs84 %>%
     title = "Flood Vulnerability",
     position = "bottomright"
   )
-map
+map1
 
 # Save final community map as a RData object (R), shapefile (ArcGIS), and KML (Google Maps) 
-community_map <- tampabay_metrics_wgs84
-#save(community_map, file = 'data/community_map.RData')
-#st_write(community_map, "data/community_map.shp", driver = "ESRI Shapefile", delete_dsn = TRUE)
-#st_write(community_map, "data/community_map.kml", driver = "KML", delete_dsn = TRUE)
+community_map1 <- tampabay_metrics1_wgs84
+#save(community_map1, file = 'data/community_map1.RData')
+#st_write(community_map1, "data/community_map1.shp", driver = "ESRI Shapefile", delete_dsn = TRUE)
+#st_write(community_map1, "data/community_map1.kml", driver = "KML", delete_dsn = TRUE)
 
 
 # Save the leaflet map as an HTML file that can be shared for easy viewing outside of R
 library(htmlwidgets)
-#saveWidget(map, "community_map.html", selfcontained = TRUE)
+#saveWidget(map1, "community_map1.html", selfcontained = TRUE)
 
 
-# ---- (ALL BURDENS) Create map of burdened communities ----
+# ---- * METHOD 2: Preferential consideration of Demographic, Economic, and Household Burdens ----
+
+tampabay_metrics2 <- tampabay_metrics %>%
+  # calculate vulnerability index based on the weights applied to the 12 vulnerability criteria
+  mutate(vulnerability = 
+           (burden_income*1) + 
+           (burden_mobile*1) + 
+           (burden_disabled*1) + 
+           (burden_tri*1) + 
+           (burden_costs*0.5) + 
+           (burden_uninsured*0.5) + 
+           (burden_plumbing*0.5) + 
+           (burden_overcrowded*0.5) + 
+           (burden_age*0.5) + 
+           (burden_language*0.5) + 
+           (burden_superfunds*0.5) + 
+           (burden_hazwaste*0.5)) %>%
+  # if there are only environmental burdens present, assign a vulnerability score of 0, otherwise keep the score
+  mutate(vulnerability = ifelse(rowSums(across(burden_income:txt_language) == 1) > 0, vulnerability, 0)) %>%
+  mutate(txt_income = ifelse(burden_income == 1, "Low income", NA),
+         txt_costs = ifelse(burden_costs == 1, "Housing cost stress", NA),
+         txt_uninsured = ifelse(burden_uninsured == 1, "Lacking health insurance", NA),
+         txt_mobile = ifelse(burden_mobile == 1, "Mobile homes", NA),
+         txt_plumbing = ifelse(burden_plumbing == 1, "Plumbing deficiencies", NA),
+         txt_overcrowded = ifelse(burden_overcrowded == 1, "Over-crowded households", NA),
+         txt_disabled = ifelse(burden_disabled == 1, "Disabled population", NA),
+         txt_age = ifelse(burden_age == 1, "Vulnerable age groups", NA),
+         txt_language = ifelse(burden_language == 1, "Linguistically isolated", NA),
+         txt_tri = ifelse(burden_tri == 1, "Proximity to toxic release sites", NA),
+         txt_superfunds = ifelse(burden_superfunds == 1, "Proximity to Superfund sites", NA),
+         txt_hazwaste = ifelse(burden_hazwaste == 1, "Proximity to hazardous waste sites", NA)) %>%
+  # list all burdens
+  mutate(vul_burdens = paste(txt_income,txt_costs,txt_uninsured,txt_mobile,txt_plumbing,txt_overcrowded,txt_disabled,
+                             txt_age,txt_language,txt_tri,txt_superfunds,txt_hazwaste, sep = ", ")) %>%
+  mutate(vul_burdens = gsub('NA, ', '', vul_burdens)) %>%
+  mutate(vul_burdens = gsub(', NA', '', vul_burdens)) %>%
+  mutate(vul_burdens = ifelse(vul_burdens == "NA", "None", vul_burdens)) %>%
+  # list burdens by dimension
+  mutate(vul_economic = paste(txt_income,txt_costs,txt_uninsured, sep = ", ")) %>%
+  mutate(vul_economic = gsub('NA, ', '', vul_economic)) %>%
+  mutate(vul_economic = gsub(', NA', '', vul_economic)) %>%
+  mutate(vul_economic = ifelse(vul_economic == "NA", "None", vul_economic)) %>%
+  mutate(vul_demographic = paste(txt_mobile,txt_plumbing,txt_overcrowded, sep = ", ")) %>%
+  mutate(vul_demographic = gsub('NA, ', '', vul_demographic)) %>%
+  mutate(vul_demographic = gsub(', NA', '', vul_demographic)) %>%
+  mutate(vul_demographic = ifelse(vul_demographic == "NA", "None", vul_demographic)) %>%
+  mutate(vul_household = paste(txt_disabled,txt_age,txt_language, sep = ", ")) %>%
+  mutate(vul_household = gsub('NA, ', '', vul_household)) %>%
+  mutate(vul_household = gsub(', NA', '', vul_household)) %>%
+  mutate(vul_household = ifelse(vul_household == "NA", "None", vul_household)) %>%
+  mutate(vul_environment = paste(txt_tri,txt_superfunds,txt_hazwaste, sep = ", ")) %>%
+  mutate(vul_environment = gsub('NA, ', '', vul_environment)) %>%
+  mutate(vul_environment = gsub(', NA', '', vul_environment)) %>%
+  mutate(vul_environment = ifelse(vul_environment == "NA", "None", vul_environment)) %>%
+  mutate(tract = as.character(tract)) %>%
+  select(-(txt_income:txt_hazwaste)) %>%
+  relocate(vulnerability, .after = burdens) %>%
+  relocate(vul_burdens, .after = vulnerability)
 
 # Convert to WGS84 for viewing with leaflet
-tampabay_metrics_wgs84 <- st_transform(tampabay_metrics, crs = 4326)
+tampabay_metrics2_wgs84 <- st_transform(tampabay_metrics2, crs = 4326)
 
 # Create color palette
-pal_burdens <- colorNumeric(palette = brewer.pal(8, "YlOrRd"), domain = tampabay_metrics_wgs84$total_burdens)
+pal_vulnerability <- colorNumeric(palette = brewer.pal(8, "YlGnBu"), domain = tampabay_metrics2_wgs84$vulnerability)
+pal_vulnerability <- colorNumeric(palette = c(brewer.pal(8, "YlGnBu"), "#000000"), domain = c(0, 8))
+#pal_vulnerability <- colorNumeric(plasma(256), domain = tampabay_metrics2_wgs84$vulnerability)
 
 # Plot the number of burdens identified, and show list of burdens upon click
-map <- tampabay_metrics_wgs84 %>%
+map2 <- tampabay_metrics2_wgs84 %>%
   leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(
-    fillColor = ~pal_burdens(total_burdens),
+    fillColor = ~pal_vulnerability(vulnerability),
     fillOpacity = 0.7,
     weight = 0.5,
     opacity = 1,
     color = "black",
-    popup = ~paste("<b>Number of Burdens</b>:", total_burdens, "<br><b>Burdens</b>:", burdens)
-  ) %>%
+    popup = ~paste0(
+      "<div style='
+    font-family: Arial, sans-serif;
+    font-size: 13px;
+    line-height: 1.4;
+    padding: 8px;
+    min-width: 220px;
+  '>",
+      # Header
+      "<div style='
+    font-weight: bold;
+    font-size: 20px;
+    margin-bottom: 4px;
+  '>Vulnerability Index</div>",
+      "<div style='
+    font-size: 20px;
+    font-weight: bold;
+    color: #2c7fb8;
+    margin-bottom: 6px;'>", 
+      vulnerability, "<span style='font-size:14px; font-weight:normal;'> / 8</span></div>",
+      # Divider
+      "<hr style='border: none; border-top: 1px solid #ddd; margin: 6px 0;'>",
+      # Sections
+      "<div style='margin-bottom:6px;'><b>Economic Burdens:</b> ", vul_economic, "</div>",
+      "<div style='margin-bottom:6px;'><b>Household Burdens:</b> ", vul_household, "</div>",
+      "<div style='margin-bottom:6px;'><b>Demographic Burdens:</b> ", vul_demographic, "</div>",
+      "<div style='margin-bottom:6px;'><b>Environmental Burdens:</b> ", vul_environment, "</div>",
+      "</div>")) %>%
   addLegend(
-    pal = pal_burdens,
-    values = ~total_burdens,
-    title = "No. of Burdens",
+    pal = pal_vulnerability,
+    values = c(0,8),
+    title = "Flood Vulnerability",
     position = "bottomright"
   )
+map2
 
 # Save final community map as a RData object (R), shapefile (ArcGIS), and KML (Google Maps) 
-community_map <- tampabay_metrics_wgs84
-#save(community_map, file = 'data/community_map.RData')
-#st_write(community_map, "data/community_map.shp", driver = "ESRI Shapefile", delete_dsn = TRUE)
-#st_write(community_map, "data/community_map.kml", driver = "KML", delete_dsn = TRUE)
+community_map2 <- tampabay_metrics2_wgs84
+#save(community_map2, file = 'data/community_map2.RData')
+#st_write(community_map2, "data/community_map2.shp", driver = "ESRI Shapefile", delete_dsn = TRUE)
+#st_write(community_map2, "data/community_map2.kml", driver = "KML", delete_dsn = TRUE)
 
 
 # Save the leaflet map as an HTML file that can be shared for easy viewing outside of R
 library(htmlwidgets)
-#saveWidget(map, "community_map.html", selfcontained = TRUE)
+#saveWidget(map2, "community_map2.html", selfcontained = TRUE)
 
